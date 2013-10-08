8a4e4db99c0fecb7f81d1978c3c72e878ada43ec
jguerinet <julien.guerinet@mail.mcgill.ca>
2013-10-07 22:30:27 -0400

    Fixed Indentation

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -9,16 +9,16 @@ import static org.junit.Assert.* ;
 
 public class TemperatureTest {
 
-  // Add multiple tests to check all functions of
-  // {@Code Temperature} class.
+	// Add multiple tests to check all functions of
+	// {@Code Temperature} class.
 
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
+	//@Test
+	//public void .... (){
+	//  ...
+	//}
 
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
+	// @Test
+	// public void .... (){
+	//    ...
+	// }
 }
