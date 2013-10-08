963257aa7765b9fdaa3eefba9ee5d0d22106d839
jguerinet <julien.guerinet@mail.mcgill.ca>
2013-10-07 22:47:14 -0400

    Added the Testing of getUnits()
    Description of test in the code.

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -12,10 +12,30 @@ public class TemperatureTest {
 	// Add multiple tests to check all functions of
 	// {@Code Temperature} class.
 
-	//@Test
-	//public void .... (){
-	//  ...
-	//}
+	//1. Check the getUnits() method
+	@Test
+	public void testGetUnits (){
+		//For testing this method, we only need to test 1 unit, and both constructors.
+		/* REASONING:
+		 * The three units are all part of an enum, and dont actually change anything apart from 
+		 * the calculation during the conversion. Since we are only looking at getUnits(), which 
+		 * essentially is a getter method, as long as the argument to the constructor is correct, 
+		 * i.e. one of the enum values, the outcome will be the same for every unit. 
+		 * 
+		 * However, we should test the 2 different constructors to ensure that the units 
+		 * do not change when the second constructor calls upon the first constructor. 
+		 */
+		
+		//Here we will be testing in Kelvin (arbitrary choice)
+		
+		//First constructor, where we input the value and the units. 
+		Temperature kelvinTemperature = new Temperature(100, Temperature.Units.KELVIN);
+		assertTrue("First Constructor: getUnits = Kelvin", kelvinTemperature.getUnits().equals(Temperature.Units.KELVIN));
+		
+		//Second constructor, where we input a previously created temperature.
+		Temperature secondKelvinTemperature = new Temperature(kelvinTemperature);
+		assertTrue("Second Constrictor: getUnits = Kelvin", secondKelvinTemperature.getUnits().equals(Temperature.Units.KELVIN));	
+	}
 
 	// @Test
 	// public void .... (){
